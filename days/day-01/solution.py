2853439b0714bc1305dc5603b016f37c85b1b564
Adam Kulpa <adamkulpa16@gmail.com>
2022-12-01 14:49:06 +0000

    publish solution code add part two problem

--- days/day-01/solution.py
+++ days/day-01/solution.py
@@ -1,12 +1,36 @@
+import pathlib
+
+currentPath = pathlib.Path(__file__).parent.resolve()
+filepath = pathlib.Path(currentPath, "input.txt")
+
 #open file
+f = open(filepath)
 
 #initialise a max variable
+maxCalorieForElf = 0
 
-#for each elf
-    #initialise a calorie count variable
+#initialise a calorie count variable
+currentElfCalorieCount = 0
 
-    #read line
+#for each line
+for line in f:
+    
+    if line != "\n":    #current elf
+        #if line has calories, add calories to current elf calorie count
+        currentElfCalorieCount += int(line)
+    else:               #new elf
+        #if line is empty, 
+        if (currentElfCalorieCount > maxCalorieForElf):
+            maxCalorieForElf = currentElfCalorieCount
+        currentElfCalorieCount = 0
 
-    #if line is empty, check against current max variable and update if needed, then new elf
+#must do this one more time for the last elf
+if (currentElfCalorieCount > maxCalorieForElf):
+    maxCalorieForElf = currentElfCalorieCount
 
-    #if line has calories, add calories to current elf calorie count
+print(f'Elf with the most calories has {maxCalorieForElf} calories!')
+
+
+    
+
+    
