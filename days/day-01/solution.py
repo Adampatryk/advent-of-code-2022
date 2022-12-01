c3ad091cf30cb85b046e4ce333f360b27a502eb4
Adam Kulpa <adamkulpa16@gmail.com>
2022-12-01 14:23:56 +0000

    problem, input, and solution file with comments

---
+++ days/day-01/solution.py
@@ -0,0 +1,12 @@
+#open file
+
+#initialise a max variable
+
+#for each elf
+    #initialise a calorie count variable
+
+    #read line
+
+    #if line is empty, check against current max variable and update if needed, then new elf
+
+    #if line has calories, add calories to current elf calorie count
